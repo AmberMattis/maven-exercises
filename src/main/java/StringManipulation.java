c128cd7494d15f19620f82b9c8d5659ed4dae662
Amber Mattis <amber.mattis2@gmail.com>
2021-01-10 19:15:34 -0600

    added title and color to cosole text

--- src/main/java/StringManipulation.java
+++ src/main/java/StringManipulation.java
@@ -1,21 +1,28 @@
 import org.apache.commons.lang3.StringUtils;
 import java.util.Scanner;
 
-public class StringManipulation {
+public class StringManipulation extends Ascii {
     public static void main(String[] args) {
+        Ascii title = new Ascii();
+
+        Ascii.asciiTitle();
 
         Scanner userInput = new Scanner(System.in);
-        System.out.println("Enter something");
-        String response = userInput.next();
+
+        System.out.println(RESET + "Enter something");
+        String response = userInput.nextLine();
+        System.out.println( );
+
+        System.out.println(YELLOW + "You Entered: " + RESET + response);
 
         if (StringUtils.isNumeric(response)) {
-            System.out.println("Response is a number.");
+            System.out.println(response + YELLOW + " is a number.");
         } else {
-            System.out.println("Response is not a number.");
+            System.out.println(response + YELLOW +  " is not a number.");
         }
 
-        System.out.println("Your response with cases swapped: " + StringUtils.swapCase(response));
-        System.out.println("Your response reversed: " + StringUtils.reverse(response));
+        System.out.println(YELLOW + "Your response with cases swapped: " + RESET +  StringUtils.swapCase(response));
+        System.out.println(YELLOW + "Your response reversed: " + RESET +  StringUtils.reverse(response));
 
     }
 }
